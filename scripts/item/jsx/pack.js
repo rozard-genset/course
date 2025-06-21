2ff6e8ffd7419c1a9526bb21133da34423877c43
almuhdilkarim <almuhdilkarim2@mail.com>
2025-06-21 17:52:12 +0700

    update

--- scripts/item/jsx/pack.js
+++ scripts/item/jsx/pack.js
@@ -1,1 +1,25 @@
 console.log('deffers course javascript engine loaded');
+
+let action_open = document.querySelectorAll(".tab-action");
+let action_aray = Array.prototype.slice.call(action_open);
+let mantab_open = document.querySelectorAll(".tabs-item");
+let mantab_aray = Array.prototype.slice.call(mantab_open);
+
+action_aray.forEach( (event) => {
+
+    event.addEventListener('click', ()=> {
+
+        let target = event.getAttribute('data-target');
+
+        action_aray.forEach( (event)=> {
+            event.classList.remove('open');
+        });
+
+        mantab_aray.forEach( (event)=> {
+            event.classList.remove('open');
+        });
+
+        event.classList.add('open');
+        document.getElementById(target).classList.add('open');        
+    }) 
+});
