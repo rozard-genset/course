3106677dd11a1641a4b02d135f0ae581b87a4ae8
almuhdilkarim <almuhdilkarim2@mail.com>
2025-08-20 15:51:46 +0700

    update

--- engine/script/page/main/jsx/pack.js
+++ engine/script/page/main/jsx/pack.js
@@ -2,6 +2,11 @@ console.log('Module : page : deffers page course javascript engine loaded');
 
 
 let init_plan = ()=> {
+
+    if ( ! document.getElementById('main') ){
+        return;
+    }
+
     if ( window.innerWidth > 1024 ) {
         let element = document.querySelector( '#plan .list' );
         let activet = element.querySelector('a.active');
@@ -38,7 +43,13 @@ let open_plan = ()=> {
 }
 
 
-let init_tabs = ()=> {
+let main_tabs = ()=> {
+
+    if ( ! document.getElementById('main') ){
+        return;
+    }
+
+
     let action_open = document.querySelectorAll(".tab-action");
     let action_aray = Array.prototype.slice.call(action_open);
     let mantab_open = document.querySelectorAll(".tabs-item");
@@ -60,75 +71,42 @@ let init_tabs = ()=> {
 }
 
 
-let init_talk = ()=> {
+let task_tabs = ()=> {
 
-    let darkmod = localStorage.getItem( 'darkmode' );
-    let contain = document.querySelector( '#forum-collumn' );
-    let frameid = contain.getAttribute('data-title');;
-    let telebox = document.createElement( 'script' );
+    if ( ! document.getElementById('task') ){
+        return;
+    }
 
-    telebox.src = 'https://comments.app/js/widget.js?3';
-    telebox.setAttribute('data-comments-app-website', '_-K1zvYW');
-    telebox.setAttribute('data-limit', 5 );
-    telebox.setAttribute('title', frameid );
-    telebox.setAttribute('data-dislikes', 1 );
+    let tabscourses = document.getElementById('task');
+    let action_open = tabscourses.querySelectorAll(".tab-action");
+    let action_aray = Array.prototype.slice.call(action_open);
+    let mantab_open = tabscourses.querySelectorAll(".tabs-item");
+    let mantab_aray = Array.prototype.slice.call(mantab_open);
 
-    if ( darkmod == 2 ) {
-        telebox.setAttribute('data-dark', 1 );
-    }
-    else if ( darkmod == 3 ) {
-        telebox.setAttribute('data-dark', 0 );
-    }
-    else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
-        telebox.setAttribute('data-dark', 1 );
-    }
-    else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
-        telebox.setAttribute('data-dark', 0 );
-    }
-    else {
-        telebox.setAttribute('data-dark', 0 );
-    }
-    contain.appendChild(telebox);
+    action_aray.forEach( (event) => {
+
+        event.addEventListener('click', ()=> {
+
+            let target = event.getAttribute('data-target');
+
+            action_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
+
+            mantab_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
+
+            event.classList.add('open');
+            document.getElementById(target).classList.add('open');        
+        }) 
+    });
 }
 
 
-let init_bugs = ()=> {
-
-    if (document.querySelector('.utterances-frame')) {
-
-        let modes = localStorage.getItem("darkmode");
-        let theme = 'github-light';
-
-        if ( modes == 3 ) {
-            theme = 'github-light';
-        }
-        else if ( modes == 2 ) {
-            theme = 'dark-blue';
-        }
-        else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ) {
-            theme = 'dark-blue';
-        }
-        else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches ) {
-            theme = 'github-light';
-        }
-        else {
-            theme = 'github-light';
-        }
-  
-        let message = {
-            type: 'set-theme',
-            theme: theme
-        };
-        
-        let iframe = document.querySelector('.utterances-frame');
-        iframe.contentWindow.postMessage(message, 'https://utteranc.es');
-    }
-}
- 
 
 window.addEventListener( 'load', ()=> {
     init_plan();
-    init_tabs();
-    // init_bugs();
-    // init_talk();
+    main_tabs();
+    task_tabs();
 });
