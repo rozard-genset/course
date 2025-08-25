ce8960a19a9b5ab9c19bf1ce1e606bdc647ae571
almuhdilkarim <almuhdilkarim2@mail.com>
2025-08-25 07:27:23 +0700

    update

--- engine/script/item/main/jsx/pack.js
+++ engine/script/item/main/jsx/pack.js
@@ -1,25 +1,62 @@
-console.log('Module : deffers main course javascript engine loaded');
+console.log( 'Module : deffers main course javascript engine loaded' );
 
-let action_open = document.querySelectorAll(".tab-action");
-let action_aray = Array.prototype.slice.call(action_open);
-let mantab_open = document.querySelectorAll(".tabs-item");
-let mantab_aray = Array.prototype.slice.call(mantab_open);
 
-action_aray.forEach( (event) => {
+let main_tabs = ()=> {
+    let action_open = document.querySelectorAll(".base-action");
+    let action_aray = Array.prototype.slice.call(action_open);
+    let mantab_open = document.querySelectorAll(".base-item");
+    let mantab_aray = Array.prototype.slice.call(mantab_open);
 
-    event.addEventListener('click', ()=> {
+    action_aray.forEach( (event) => {
 
-        let target = event.getAttribute('data-target');
+        event.addEventListener('click', ()=> {
 
-        action_aray.forEach( (event)=> {
-            event.classList.remove('open');
-        });
+            let target = event.getAttribute('data-target');
 
-        mantab_aray.forEach( (event)=> {
-            event.classList.remove('open');
-        });
+            action_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
 
-        event.classList.add('open');
-        document.getElementById(target).classList.add('open');        
-    }) 
-});
+            mantab_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
+
+            event.classList.add('open');
+            document.getElementById(target).classList.add('open');        
+        }) 
+    });
+}
+
+
+let node_tabs = ()=> {
+    let parent_elem = document.getElementById('node');
+    let action_open = parent_elem.querySelectorAll(".node-action");
+    let action_aray = Array.prototype.slice.call(action_open);
+    let mantab_open = parent_elem.querySelectorAll(".node-item");
+    let mantab_aray = Array.prototype.slice.call(mantab_open);
+
+    action_aray.forEach( (event) => {
+
+        event.addEventListener('click', ()=> {
+
+            let target = event.getAttribute('data-target');
+
+            action_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
+
+            mantab_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
+
+            event.classList.add('open');
+            document.getElementById(target).classList.add('open');        
+        }) 
+    });
+}
+
+
+window.addEventListener('load', ()=> {
+    main_tabs();
+    node_tabs();
+})
--- engine/script/item/mile/jsx/pack.js
+++ engine/script/item/mile/jsx/pack.js
@@ -1,14 +1,42 @@
 console.log('deffers paid course javascript engine loaded');
 
+let plan_tabs = ()=> {
+    let plansession = Array.prototype.slice.call(document.querySelectorAll('.period'));
 
-let plansession = Array.prototype.slice.call(document.querySelectorAll('.plan-session'));
-
-plansession.forEach((item)=> {
+    plansession.forEach((item)=> {
 
 
-    let action_open = item.querySelectorAll(".tab-action");
+        let action_open = item.querySelectorAll(".tab-action");
+        let action_aray = Array.prototype.slice.call(action_open);
+        let mantab_open = item.querySelectorAll(".tabs-item");
+        let mantab_aray = Array.prototype.slice.call(mantab_open);
+
+        action_aray.forEach( (event) => {
+
+            event.addEventListener('click', ()=> {
+
+                let target = event.getAttribute('data-target');
+
+                action_aray.forEach( (event)=> {
+                    event.classList.remove('open');
+                });
+
+                mantab_aray.forEach( (event)=> {
+                    event.classList.remove('open');
+                });
+
+                event.classList.add('open');
+                document.getElementById(target).classList.add('open');        
+            }) 
+        });
+    })
+}
+
+let node_tabs = ()=> {
+    let parent_elem = document.getElementById('node');
+    let action_open = parent_elem.querySelectorAll(".node-action");
     let action_aray = Array.prototype.slice.call(action_open);
-    let mantab_open = item.querySelectorAll(".tabs-item");
+    let mantab_open = parent_elem.querySelectorAll(".node-item");
     let mantab_aray = Array.prototype.slice.call(mantab_open);
 
     action_aray.forEach( (event) => {
@@ -29,6 +57,36 @@ plansession.forEach((item)=> {
             document.getElementById(target).classList.add('open');        
         }) 
     });
+}
+
+let main_tabs = ()=> {
+    let action_open = document.querySelectorAll(".base-action");
+    let action_aray = Array.prototype.slice.call(action_open);
+    let mantab_open = document.querySelectorAll(".base-item");
+    let mantab_aray = Array.prototype.slice.call(mantab_open);
+
+    action_aray.forEach( (event) => {
+
+        event.addEventListener('click', ()=> {
+
+            let target = event.getAttribute('data-target');
+
+            action_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
+
+            mantab_aray.forEach( (event)=> {
+                event.classList.remove('open');
+            });
+
+            event.classList.add('open');
+            document.getElementById(target).classList.add('open');        
+        }) 
+    });
+}
+
+window.addEventListener('load', ()=> {
+    plan_tabs();
+    main_tabs();
+    node_tabs();
 })
-
-
